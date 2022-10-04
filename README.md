7e84cbd6a28967ecda1dd4e163f42d39315d3261
Liz Burton <liz.burton147@gmail.com>
2022-10-04 08:31:16 -0400

    draft lab

---
+++ README.md
@@ -0,0 +1,90 @@
+# Cash Register Lab
+
+## Learning Goals
+
+- Build a class with instance methods.
+- Call instance methods inside of other instance methods.
+- Use instance methods to track information pertinent to an instance of a class.
+
+***
+
+## Key Vocab
+
+- **Class**: a bundle of data and functionality. Can be copied and modified to
+accomplish a wide variety of programming tasks.
+- **Initialize**: create a working copy of a class using its `__init__`
+method.
+- **Instance**: one specific working copy of a class. It is created when a
+class's `__init__` method is called.
+- **Object**: the more common name for an instance. The two can usually be used
+interchangeably.
+- **Object-Oriented Programming**: programming that is oriented around data
+(made mobile and changeable in **objects**) rather than functionality. Python
+is an object-oriented programming language.
+- **Function**: a series of steps that create, transform, and move data.
+- **Method**: a function that is defined inside of a class.
+- **Magic Method**: a special type of method in Python that starts and ends
+with double underscores. These methods are called on objects under certain
+conditions without needing to use their names explicitly. Also called **dunder
+methods** (for **d**ouble **under**score).
+- **Attribute**: variables that belong to an object.
+- **Property**: attributes that are controlled by methods.
+
+***
+
+## Introduction
+
+We're going to create an Object-Oriented Cash Register that can:
+
+- Add items of varying quantities and prices
+- Calculate discounts
+- Keep track of what's been added to it
+- Void the last transaction
+
+## Instructions
+
+**This is a test-driven lab!** You will need to read the spec file and the test
+output very carefully to solve this one.
+
+Note that a **discount** is calculated as a percentage off of the total cash
+register price (e.g. a discount of 20 means the customer receives 20% off of
+their total price).
+
+**Hint #1:** Keep in mind that to access an attribute or call an instance method
+_inside_ another instance method, we use the `self` keyword to refer to the
+instance on which we are operating. For example:
+
+```py
+class Person:
+
+  def __init__(age = 0):
+    @age = age
+
+  def birthday(self):
+    self.age += 1
+```
+
+Follow along with the specs in `lib/testing/cash_register_test.py`. Reading
+along with what the tests are looking for can be really helpful!
+
+Take it one step at a time!
+
+**Hint #2:** The `apply_discount()` method requires some knowledge about working
+with integers versus floats in Python. When you get to that method, take a look
+at what return value the tests are expecting and keep in mind that Python
+provides methods for changing an Integer to a Float and vice versa.
+
+**Hint #3:** The `void_last_transaction()` method will remove the last
+transaction from the total. You'll need to make an additional attribute and keep
+track of that last transaction amount somehow. In what method of the class are
+you working with an individual item?
+
+## Resources
+
+- [String `replace()` method][replace]
+- [String `split()` method][split]
+- [re module][re]
+
+[replace]: https://docs.python.org/3/library/stdtypes.html?highlight=replace#str.replace
+[split]: https://docs.python.org/3/library/stdtypes.html?highlight=split#str.split
+[re]: https://docs.python.org/3/library/re.html
---
+++ testing/cash_register_test.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+from lib.cash_register import CashRegister
+
+import io
+import sys
+
+class TestCashRegister:
+    '''CashRegister in cash_register.py'''
+
+    cash_register = CashRegister()
+    cash_register_with_discount = CashRegister(20)
+
+    def reset_register_totals(self):
+      self.cash_register.total = 0
+      self.cash_register_with_discount.total = 0
+
+    def test_discount_attribute(self):
+        '''takes one optional argument, a discount, on initialization.'''
+        assert(self.cash_register.discount == 0)
+        assert(self.cash_register_with_discount.discount == 20)
+
+    def test_total_attribute(self):
+        '''sets an instance variable total on initialization to zero.'''
+        assert(self.cash_register.total == 0)
+        assert(self.cash_register_with_discount.total == 0)
+
+    def test_add_item(self):
+        '''accepts a title and a price and increases the total.'''
+        self.cash_register.add_item("eggs", 0.98)
+        assert(self.cash_register.total == 0.98)
+        # self.reset_total(self.cash_register)
+        self.reset_register_totals()
+
+    def test_add_item_optional_quantity(self):
+        '''also accepts an optional quantity.'''
+        self.cash_register.add_item("book", 5.00, 3)
+        assert(self.cash_register.total == 15.00)
+        # self.cash_register.total = 0
+        self.reset_register_totals()
+
+    def test_add_item_with_multiple_items(self):
+        '''doesn"t forget about the previous total'''
+        self.cash_register.add_item("Lucky Charms", 4.5)
+        assert(self.cash_register.total == 4.5)
+        self.cash_register.add_item("Ritz Crackers", 5.0)
+        assert(self.cash_register.total == 9.5)
+        self.cash_register.add_item("Justin's Peanut Butter Cups", 2.50, 2)
+        assert(self.cash_register.total == 14.5)
+        self.reset_register_totals()
+
+    def test_apply_discount(self):
+        '''applies the discount to the total price.'''
+        self.cash_register_with_discount.add_item("macbook air", 1000)
+        self.cash_register_with_discount.apply_discount()   
+        assert(self.cash_register_with_discount.total == 800)
+        # self.cash_register_with_discount.total = 0
+        self.reset_register_totals()
+
+    def test_apply_discount_success_message(self):
+        '''returns success message with updated total'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        self.cash_register_with_discount.add_item("macbook air", 1000)
+        self.cash_register_with_discount.apply_discount()
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "After the discount, the total comes to $800.\n")
+        self.reset_register_totals()
+
+    def test_apply_discount_reduces_total(self):
+        '''reduces the total'''
+        self.cash_register_with_discount.add_item("macbook air", 1000)
+        self.cash_register_with_discount.apply_discount()
+        assert(self.cash_register_with_discount.total == 800)
+        self.reset_register_totals()
+
+    def test_apply_discount_when_no_discount(self):
+        '''returns a string error message that there is no discount to apply'''
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        self.cash_register.apply_discount()
+        sys.stdout = sys.__stdout__
+        assert(captured_out.getvalue() == "There is no discount to apply.\n")
+        self.reset_register_totals()
+
+    def test_items_list_without_multiples(self):
+        '''returns an array containing all items that have been added'''
+        new_register = CashRegister()
+        new_register.add_item("eggs", 1.99)
+        new_register.add_item("tomato", 1.76)
+        assert(new_register.items == ["eggs", "tomato"])
+
+    def test_items_list_with_multiples(self):
+        '''returns an array containing all items that have been added, including multiples'''
+        new_register = CashRegister()
+        new_register.add_item("eggs", 1.99, 2)
+        new_register.add_item("tomato", 1.76, 3)
+        assert(new_register.items == ["eggs", "eggs", "tomato", "tomato", "tomato"])
+
+    def test_void_last_transaction(self):
+      '''subtracts the last item from the total'''
+      self.cash_register.add_item("apple", 0.99)
+      self.cash_register.add_item("tomato", 1.76)
+      self.cash_register.void_last_transaction()
+      assert(self.cash_register.total == 0.99)
+      self.reset_register_totals()
+
+    def test_void_last_transaction_with_multiples(self):
+      '''returns the total to 0.0 if all items have been removed'''
+      self.cash_register.add_item("tomato", 1.76, 2)
+      self.cash_register.void_last_transaction() 
+      assert(self.cash_register.total == 0.0)
+      self.reset_register_totals()
+      
