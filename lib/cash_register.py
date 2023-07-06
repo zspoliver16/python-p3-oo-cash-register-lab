34d4caf1423a870ceb53b28f00f76a6998d883c7
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-07-06 17:23:00 -0400

    Revert "code"
    This reverts commit 5172154a3712ce76411e7b0f21b5233ccaad19a9.
    
    reverting changes

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,28 +1,4 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  
-  def __init__(self,discount=0):
-    self.discount=discount
-    self.total=0
-    self.items=[]
-    self.last_transaction={}
-
-  def add_item(self,item,amount,quantity=1):
-    self.total+=(amount*quantity)
-    for i in range(quantity):
-      self.items.append(item)
-    self.last_transaction['item']=item
-    self.last_transaction['amount']=amount
-    self.last_transaction['quantity']=quantity
-
-  def apply_discount(self):
-    if not self.discount:
-      print("There is no discount to apply.")
-    else:
-      self.total=self.total-self.total*(self.discount/100)
-      print(f"After the discount, the total comes to ${int(self.total)}.")
-
-  def void_last_transaction(self):
-    self.items.pop()
-    self.total-=self.last_transaction['amount']*self.last_transaction['quantity']
+  pass
