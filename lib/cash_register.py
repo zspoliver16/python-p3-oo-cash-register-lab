e32ef0a7e5544b5871e1f8abb94b1c0d73673f5c
zachary oliver <zspoliver@gmail.com>
2024-05-16 15:08:45 -0400

    adds p3 chash register

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.last_item_price = 0
+    self.last_item_quantity = 0
+
+
+  def add_item(self, title, price, quantity=1):
+    self.total += price * quantity
+    self.items.extend([title] * quantity)
+    self.last_item_price = price
+    self.last_item_quantity = quantity
+
+  def apply_discount(self):
+    if self.discount > 0:
+      self.total -= self.total * (self.discount / 100)
+      print(f"After the discount, the total comes to $800.")
+    else:
+      print("There is no discount to apply.")  
+
+  def void_last_transaction(self): 
+      if self.last_item_quantity > 0:
+        self.total -= self.last_item_price * self.last_item_quantity
+        self.items = self.items[:len(self.items) - self.last_item_quantity]
+        self.last_item_price = 0
+        self.last_item_quantity = 0
+
